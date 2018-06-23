d78f4f8b63074f106d65b7f10fc2781ef9274348
MrMarosis <bartlomiej.t.stachura@gmail.com>
2018-06-23 13:36:06 +0200

    Add function generation

--- Lab5/harmonic_average.jl
+++ Lab5/harmonic_average.jl
@@ -1,7 +1,11 @@
-function harmonicAV(parts...)
+function init_harmonicAV(parts...)
     sum= :(0)
     for i in parts
         sum= :($sum+(1/$i))
     end
     return :(length($parts)/$sum)
+end
+
+@generated function harmonicAV(parts...)
+    return init_harmonicAV(parts)
 end
